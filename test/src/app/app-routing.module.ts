0b9ba876d337b41aa82d8697babfe22ee9571101
Kiendrebeogo Hugues (s124523) <hugues.kiendrebeogo@student.ap.be>
2021-12-05 20:25:18 +0100

    route to column

--- test/src/app/app-routing.module.ts
+++ test/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ColumnComponent } from './column/column.component';
 import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
-  {path:'table', component: TableComponent}
+  {path:'table', component: TableComponent},
+  {path:'column',component: ColumnComponent}
 ];
 
 @NgModule({
