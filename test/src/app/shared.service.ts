32f1f279ea8323193a99b3180441e1fa07b1752b
Kiendrebeogo Hugues (s124523) <hugues.kiendrebeogo@student.ap.be>
2021-12-07 20:31:35 +0100

    typo fixed

--- test/src/app/shared.service.ts
+++ test/src/app/shared.service.ts
@@ -14,7 +14,7 @@ export class SharedService {
     return this.http.get < any > (this.APIUrl+'/Tables/getTableNames');
 }
 
-  selectDatabase(CountryCode: string): Observable<any>{
+  selectedDatabase(CountryCode: string): Observable<any>{
     return this.http.post(this.APIUrl + "/Tables/Connect?CountryCode=" + CountryCode,"");
   }
 
