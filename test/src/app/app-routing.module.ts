2cf1f275c39cd93f7b07589115f007c6f69b1804
Kiendrebeogo Hugues (s124523) <hugues.kiendrebeogo@student.ap.be>
2021-12-07 21:05:36 +0100

    added routing

--- test/src/app/app-routing.module.ts
+++ test/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatabaseComponent } from './database/database.component';
 import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
-  {path:'table', component: TableComponent}
+  {path:'table', component: TableComponent},
+  {path:'database',component:DatabaseComponent}
 ];
 
 @NgModule({
